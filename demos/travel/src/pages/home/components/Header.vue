ed87486c1c2ba38bf3e79bd132104ad11bcc42ad
storylei <storylei@126.com>
2020-06-20 21:13:07 +0800

    #demo# travel index
    项目首页Header和Swiper开发

---
+++ demos/travel/src/pages/home/components/Header.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="header">
+        <div class="header-left"><span class="iconfont">&#xe658;</span></div>
+        <div class="header-input"><span class="iconfont">&#xe63c;</span>输入城市</div>
+        <div class="header-right">城市<span class="iconfont">&#xe65c;</span></div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'HomeHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/variables.styl'
+.header
+    display flex
+    line-height .86rem
+    background $bgColor
+    color #fff;
+    .header-left
+        width .64rem
+        height .86rem
+        line-height .86rem
+        text-align center
+        .iconfont
+            font-size .5rem
+    .header-input
+        flex 1
+        margin-top .12rem
+        margin-left .2rem
+        padding-left .2rem
+        height .64rem
+        line-height .64rem
+        background-color #fff
+        border-radius .1rem
+        color #ccc
+    .header-right
+        width 1.24rem
+        text-align center
+        height .86rem
+        line-height .86rem
+        .iconfont
+            font-size .4rem
+            vertical-align bottom
+</style>
---
+++ demos/travel/src/pages/home/components/Swiper.vue
@@ -0,0 +1,51 @@
+<template>
+<div class="wrapper">
+    <swiper :options="swiperOption" ref="mySwiper">
+        <!-- slides -->
+        <swiper-slide v-for="item of swiperList" :key="item.id">
+            <img :src="item.imgUrl" alt="" class="swiper-img">
+        </swiper-slide>
+        <!-- Optional controls -->
+        <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'HomeSwiper',
+    data () {
+        return {
+            swiperOption: {
+                pagination: '.swiper-pagination',
+                loop: true
+            },
+            swiperList: [
+                {
+                    id: 1,
+                    imgUrl: 'https://github.surmon.me/images/example/5.jpg'
+                },
+                {
+                    id: 2,
+                    imgUrl: 'https://github.surmon.me/images/example/7.jpg'
+                }
+            ]
+        }
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+.wrapper >>> .swiper-pagination-bullet-active
+    background-color #fff
+.wrapper
+    width 100%
+    height 0
+    overflow hidden
+    padding-bottom 31.25%
+    background-color #ccc
+    // height 31.25vw
+    .swiper-img
+        width 100%
+        height: 31.25vw
+</style>
